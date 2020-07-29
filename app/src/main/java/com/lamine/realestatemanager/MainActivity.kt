d4337181fb91ab0359440f16370314b82b2dfc80
lamine messaci <lamine.messaci18@gmail.com>
2020-07-29 16:39:56 +0200

    init

---
+++ app/src/main/java/com/lamine/realestatemanager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lamine.realestatemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
