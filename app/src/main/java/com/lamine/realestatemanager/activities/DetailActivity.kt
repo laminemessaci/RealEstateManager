c58f5855753b2063aa3fd8c2d998250e545ca775
lamine messaci <lamine.messaci18@gmail.com>
2020-07-30 20:43:50 +0200

    git ignore file

---
+++ app/src/main/java/com/lamine/realestatemanager/activities/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.lamine.realestatemanager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lamine.realestatemanager.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
