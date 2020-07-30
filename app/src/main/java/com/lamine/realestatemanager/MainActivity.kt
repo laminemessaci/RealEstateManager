5545ebfcfdd2b5bb72b82f4a440e9df216c2f6ea
lamine messaci <lamine.messaci18@gmail.com>
2020-07-30 11:28:45 +0200

    Toolbar configuration

--- app/src/main/java/com/lamine/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/lamine/realestatemanager/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         configureToolbar()
+        configureNavDrawer()
 
 
+    }
+
+// Navigation Drawer Configuration
+    private fun configureNavDrawer() {
+        drawer = activity_main_drawer_layout
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            (R.string.navigation_drawer_open),
+            (R.string.navigation_drawer_close))
+    drawer.addDrawerListener(toggle)
     }
 
     // Toolbar configuration
