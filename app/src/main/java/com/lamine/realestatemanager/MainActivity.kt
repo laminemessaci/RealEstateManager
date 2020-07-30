93114eee1ec492dcd894fd42f917d91d1772e46a
lamine messaci <lamine.messaci18@gmail.com>
2020-07-30 11:06:07 +0200

    Toolbar configuration

--- app/src/main/java/com/lamine/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/lamine/realestatemanager/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureToolbar()
 
+
+    }
+
+    // Toolbar configuration
+    private fun configureToolbar() {
+        setSupportActionBar(toolbar)
     }
 }
