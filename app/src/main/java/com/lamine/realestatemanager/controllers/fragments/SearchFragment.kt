c4dc9bdcd613ba0884e34c4c2e51d3f4dcdd66fe
lamine messaci <lamine.messaci18@gmail.com>
2020-08-14 18:27:41 +0200

    Configure images number editText
    Signed-off-by: lamine messaci <lamine.messaci18@gmail.com>

--- app/src/main/java/com/lamine/realestatemanager/controllers/fragments/SearchFragment.kt
+++ app/src/main/java/com/lamine/realestatemanager/controllers/fragments/SearchFragment.kt
@@ -281,4 +281,26 @@ class SearchFragment : Fragment(){
         })
     }
 
+    // To configure images number editText
+    private fun configureNbrOfImages() {
+        edit_nbr_images.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                val nbrImgStr: String = edit_nbr_images.text.toString()
+                if (nbrImgStr.isNotEmpty()) {
+                    numberOfImages = nbrImgStr.toInt()
+                }
+            }
+        })
+    }
+
 }
