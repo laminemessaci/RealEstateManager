05866fb337db000765a840246ce27342b87ab20b
lamine messaci <lamine.messaci18@gmail.com>
2020-09-01 20:06:12 +0200

    Add StartUpActivity.kt to delete PropertyTest
    Signed-off-by: lamine messaci <lamine.messaci18@gmail.com>

---
+++ app/src/main/java/com/lamine/realestatemanager/controllers/activities/StartUpActivity.kt
@@ -0,0 +1,44 @@
+package com.lamine.realestatemanager.controllers.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.lifecycle.ViewModelProviders
+import com.lamine.realestatemanager.R
+import com.lamine.realestatemanager.controllers.viewModel.DataInjection
+import com.lamine.realestatemanager.controllers.viewModel.PropertyViewModel
+
+class StartUpActivity : AppCompatActivity() {
+
+    private lateinit var propertyViewModel: PropertyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+
+        deletIfExist()
+        Handler().postDelayed({
+            lanchMainActivity()
+        }, 800)
+    }
+
+    private fun lanchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
+    }
+
+    private fun deletIfExist() {
+        propertyViewModel = ViewModelProviders.of(
+            this,
+            this.let {DataInjection.Injection.provideViewModelFactory(it) }
+        ).get(PropertyViewModel::class.java)
+        Thread{
+            propertyViewModel.deleteProperty(10001)
+        }.start()
+    }
+
+
+}
+
